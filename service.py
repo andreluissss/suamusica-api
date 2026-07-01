f6c2d2a40b21e011ef1544e386f43040d142541c
andreluissss <andreluisss12345@gmail.com>
2026-06-30 22:49:20 -0300

    Use extract_flat=True to avoid format issues

--- service.py
+++ service.py
@@ -138,7 +138,7 @@ class YouTubeService:
                     'no_warnings': True,
                     'download': False,
                     'postprocessors': [],
-                    'extract_flat': False,
+                    'extract_flat': True,
                     'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
                 }
                 
